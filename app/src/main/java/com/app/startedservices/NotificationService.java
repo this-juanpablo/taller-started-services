a070cb1f6b0e1b1e7bf8a2338852cb00eb93cf74
Juan Pablo Barrera <juanpablobxp99@gmail.com>
2021-01-23 21:17:16 -0500

    Se modifica logica para crear notificaciones

--- app/src/main/java/com/app/startedservices/NotificationService.java
+++ app/src/main/java/com/app/startedservices/NotificationService.java
@@ -1,9 +1,15 @@
 package com.app.startedservices;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
-import androidx.core.app.NotificationCompat;
+import androidx.annotation.RequiresApi;
 
 public class NotificationService extends IntentService {
 
@@ -11,19 +17,33 @@ public class NotificationService extends IntentService {
         super("NotificationService");
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onHandleIntent(Intent intent) {
-        String title = intent.getStringExtra("title");
-        String content = intent.getStringExtra("content");
+        synchronized (this) {
+            String title = intent.getStringExtra("title");
+            String content = intent.getStringExtra("content");
+            try {
+                wait(5000);
+                Intent intent2 = new Intent(getApplicationContext(), MainActivity.class);
+                String CHANNEL_ID = "CHANNEL";
+                NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "name", NotificationManager.IMPORTANCE_LOW);
+                PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 1, intent2, 0);
+                Notification notification = new Notification.Builder(getApplicationContext(), CHANNEL_ID)
+                        .setContentText(content)
+                        .setContentTitle(title)
+                        .setContentIntent(pendingIntent)
+                        .addAction(android.R.drawable.sym_action_chat, "Notificación StartedServices", pendingIntent)
+                        .setChannelId(CHANNEL_ID)
+                        .setSmallIcon(android.R.drawable.sym_action_chat)
+                        .build();
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "my_channel_id_01")
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentText(content)
-                .setContentTitle(title)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setVibrate(new long[]{0, 1000})
-                .setAutoCancel(true);
-        builder.build();
-
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                notificationManager.createNotificationChannel(notificationChannel);
+                notificationManager.notify(1, notification);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
